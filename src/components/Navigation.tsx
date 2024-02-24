7dddcf3b367d83c290c536dc4bbb78ee2ffba053
axlkun <axelcruz.dev@gmail.com>
2024-02-24 15:51:05 -0600

    add link to navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,27 +1,25 @@
+import { Link } from "react-router-dom";
 
-const Login = () => {
-
-    return (
-        <header>
-        <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-          <div className="container-fluid">
-            <a className="navbar-brand" href="#">Home</a>
-
-            <div>
-              <ul className="navbar-nav me-auto mb-2 mb-md-0">
-                <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="#">Login</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="#">Register</a>
-                </li>
-              </ul>
-            </div>
+const Navigation = () => {
+  return (
+    <header>
+      <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">Home</Link> {/* Reemplaza el enlace "Home" con Link */}
+          <div>
+            <ul className="navbar-nav me-auto mb-2 mb-md-0">
+              <li className="nav-item">
+                <Link className="nav-link active" aria-current="page" to="/login">Login</Link> {/* Reemplaza el enlace "Login" con Link */}
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link active" aria-current="page" to="/register">Register</Link> {/* Reemplaza el enlace "Register" con Link */}
+              </li>
+            </ul>
           </div>
-        </nav>
-      </header>
-    )
-  }
-  
-  export default Login
-  
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navigation;
