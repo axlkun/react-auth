f2719ce245a209bf162671c893dbbcf9033f74a8
axlkun <axelcruz.dev@gmail.com>
2024-02-24 15:31:02 -0600

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,29 @@
 import './App.css'
 import Login from './pages/Login'
 import Navigation from './components/Navigation'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Register from './pages/Register'
 
 function App() {
 
   return (
-    <>
 
-      <div>
+    <div className='App'>
 
-        <Navigation/>
+      <Navigation />
 
-        <main>
-          <Login />
-        </main>
-      </div>
+      <main>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+          </Routes>
+        </BrowserRouter>
+      </main>
+    </div>
 
-    </>
   )
 }
 
