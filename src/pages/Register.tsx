57caa8a95f3e2e146740a0fdb0e70a5cc6144f44
axlkun <axelcruz.dev@gmail.com>
2024-02-25 18:26:26 -0600

    send data to backend and redirect to login

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,27 +1,62 @@
+import { SyntheticEvent, useState, useEffect } from "react"
+import { useNavigate  } from "react-router-dom";
 
 const Register = () => {
 
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [redirectPage, setRedirectPage] = useState(false);
+    const navigate = useNavigate();
+
+    const registerEndpoint = 'http://127.0.0.1:8000/api/register';
+
+    const submit = async (e: SyntheticEvent) => {
+        e.preventDefault();
+
+        await fetch(registerEndpoint, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                name,
+                email,
+                password
+            })
+        });
+
+
+        setRedirectPage(true);
+
+    }
+
+    useEffect(() => {
+        if (redirectPage) {
+            navigate('/login');
+        }
+    }, [redirectPage, navigate]);
+
     return (
-      <form>
+        <form>
             <div>
-              <h1 className='h3 mb-3 fw-normal'>Please register</h1>
+                <h1 className='h3 mb-3 fw-normal'>Please register</h1>
             </div>
             <div className="mb-3">
-              <label className="form-label">Name</label>
-              <input className="form-control" id="exampleInputName" aria-describedby="nameHelp"/>
+                <label className="form-label">Name</label>
+                <input className="form-control" id="exampleInputName" aria-describedby="nameHelp" onChange={e => setName(e.target.value)} />
             </div>
             <div className="mb-3">
-              <label className="form-label">Email address</label>
-              <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                <label className="form-label">Email address</label>
+                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" onChange={e => setEmail(e.target.value)} />
             </div>
             <div className="mb-3">
-              <label className="form-label">Password</label>
-              <input type="password" className="form-control" id="exampleInputPassword1"/>
+                <label className="form-label">Password</label>
+                <input type="password" className="form-control" id="exampleInputPassword1" onChange={e => setPassword(e.target.value)} />
             </div>
-            <button type="submit" className="btn btn-primary">Submit</button>
-          </form>
+            <button type="submit" className="btn btn-primary" onClick={submit}>Submit</button>
+        </form>
     )
-  }
-  
-  export default Register
-  
+}
+
+export default Register
