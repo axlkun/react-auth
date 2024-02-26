f3b27a3e22fed8a91b0d8c84d7be3640fc9b20b6
axlkun <axelcruz.dev@gmail.com>
2024-02-25 21:01:43 -0600

    request to the backend

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,22 +1,55 @@
+import { SyntheticEvent, useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
 
-  return (
-    <form>
-          <div>
-            <h1 className='h3 mb-3 fw-normal'>Please sign in</h1>
-          </div>
-          <div className="mb-3">
-            <label className="form-label">Email address</label>
-            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-          </div>
-          <div className="mb-3">
-            <label className="form-label">Password</label>
-            <input type="password" className="form-control" id="exampleInputPassword1"/>
-          </div>
-          <button type="submit" className="btn btn-primary">Submit</button>
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [redirectPage, setRedirectPage] = useState(false);
+    const navigate = useNavigate();
+
+    const loginEndpoint = 'http://127.0.0.1:8000/api/login';
+
+    const submit = async (e: SyntheticEvent) => {
+        e.preventDefault();
+
+        await fetch(loginEndpoint, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            credentials: 'include',
+            body: JSON.stringify({
+                email,
+                password
+            })
+        });
+
+        setRedirectPage(true);
+    }
+
+    useEffect(() => {
+        if (redirectPage) {
+            navigate('/');
+        }
+    }, [redirectPage, navigate]);
+
+    return (
+        <form onSubmit={submit}>
+            <div>
+                <h1 className='h3 mb-3 fw-normal'>Please sign in</h1>
+            </div>
+            <div className="mb-3">
+                <label className="form-label">Email address</label>
+                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" onChange={e => setEmail(e.target.value)} required />
+            </div>
+            <div className="mb-3">
+                <label className="form-label">Password</label>
+                <input type="password" className="form-control" id="exampleInputPassword1" onChange={e => setPassword(e.target.value)} required/>
+            </div>
+            <button type="submit" className="btn btn-primary">Submit</button>
         </form>
-  )
+    )
 }
 
 export default Login
